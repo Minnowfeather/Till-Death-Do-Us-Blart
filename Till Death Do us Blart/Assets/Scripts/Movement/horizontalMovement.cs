419d613ec2a56a6ffb288830c58e3fddcac6ff7d
Minnowfeather <35814311+Minnowfeather@users.noreply.github.com>
2020-01-22 20:42:58 -0800

    Initial commit

---
+++ Till Death Do us Blart/Assets/Scripts/Movement/horizontalMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class horizontalMovement : MonoBehaviour {
+
+	private Vector2 v;
+	private float x;
+	private Rigidbody2D body;
+	// Use this for initialization
+	void Start () {
+		v = new Vector2 (0.0f, 0.0f);
+		x = 0.0f;
+		body = this.GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey ("a")) {
+			x -= 50;
+		}
+		if (Input.GetKey ("d")) {
+			x += 50f;
+		}
+		v = new Vector2 (x, 0.0f);
+		body.AddForce (v);
+		x = 0;
+
+	}
+}
---
+++ Till Death Do us Blart/Assets/Scripts/Movement/jump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour {
+
+	private Vector2 v;
+	private float force;
+	private Rigidbody2D body;
+	private float oldBodyY;
+	// Use this for initialization
+	void Start () {
+		v = new Vector2 (0.0f, 0.0f);
+		force = 0.0f;
+		body = this.GetComponent<Rigidbody2D>();
+		oldBodyY = body.position.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if ((Input.GetKey ("space")) && (body.position.y == oldBodyY)) {
+			force += 5;
+		}
+		v = new Vector2 (0.0f, force);
+		body.AddForce (v, ForceMode2D.Impulse);
+		force = 0;
+		oldBodyY = body.position.y;
+
+	}
+}

ac33cae75cb24a709ff8f3ef3a05e2a23e2bf627
Underslash12 <stew_smith@hotmail.com>
2020-02-03 20:49:38 -0800

    Ehh jump, needs (lots of) work

--- Till Death Do us Blart/Assets/Scripts/Movement/jump2.cs
+++ Till Death Do us Blart/Assets/Scripts/Movement/jump2.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class jump2 : MonoBehaviour
 {
+
+    GameObject player;
+    bool onGround;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Test Obj");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("space") && onGround) {
+            player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
+        }
     }
 
-    void OnCollisionEnter2D (Collision2D col)
+    void OnCollisionStay2D (Collision2D col)
     {
-        if (Physics2D.Raycast(this.transform.position, new Vector2(0, -1), 1)) {
-            print("h2");
-        } else {
-            print("h3");
+        RaycastHit2D r = Physics2D.Raycast(this.transform.position, Vector2.down, 1);
+        /*print(r.fraction);*/
+        /*if (r.fraction > 0) {
+            print("collided");
         }
+        else {
+            print(r.fraction);
+        }*/
+        onGround = r.fraction > 0;
     }
 }

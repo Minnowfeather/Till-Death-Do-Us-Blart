77945a237ef44858511c04d73d44535e6396b534
Minnowfeather <35814311+Minnowfeather@users.noreply.github.com>
2020-07-27 22:05:23 -0700

    backed up assets

---
+++ OLD ASSETS/Scripts/Movement/jump2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump2 : MonoBehaviour
+{
+
+    GameObject player;
+    bool onGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Test Obj");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space") && onGround) {
+            player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionStay2D (Collision2D col)
+    {
+        RaycastHit2D r = Physics2D.Raycast(this.transform.position, Vector2.down, 1);
+        /*print(r.fraction);*/
+        /*if (r.fraction > 0) {
+            print("collided");
+        }
+        else {
+            print(r.fraction);
+        }*/
+        onGround = r.fraction > 0;
+    }
+}
